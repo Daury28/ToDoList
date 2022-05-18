5ecf8cdd364444ad2106b546003b05c8ca9be8e1
daury <daurymichelle@gmail.com>
2022-05-18 18:57:30 +0200

    Se agrega el Proyecto

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<div class="container">
+    <h1>Lista de Tareas</h1>
+    <div class="search">
+        <form>
+            <input type="text" placeholder="Agregar tarea...">
+            <button class="btn-add">+</button>
+        </form>
+    </div>
+    <div class="li-container">
+        <ul>
+            
+        </ul>
+    </div>
+    <div class="empty">
+        <p>No tienes tareas pendientes.</p>
+    </div>
+</div>
